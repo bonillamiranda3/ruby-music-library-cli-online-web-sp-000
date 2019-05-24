1295f96ee5ebeeeb5e9e2c0d990b304fddcb8435
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-24 17:53:06 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,33 +1,34 @@
 class Genre
-
   attr_accessor :name, :songs
   extend Concerns::Findable
+  
   @@all = []
-
+  
   def initialize(name)
     @name = name
     @songs = []
+    self.save
   end
-
+  
   def self.all
     @@all
   end
-
-  def self.destroy_all
-    @@all.clear
-  end
-
+  
   def save
     @@all << self unless @@all.include?(self)
   end
-
+  
+  def self.destroy_all
+    @@all.clear
+  end
+  
   def self.create(name)
     new = Genre.new(name)
-    new_genre.save
-    new_genre
+    new.save
+    new
   end
-
+  
   def artists
-    self.songs.map{ |song| song.artist }.uniq
+    self.songs.map { |song| song.artist }.uniq
   end
 end
