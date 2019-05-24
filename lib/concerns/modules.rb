1042b6f4d71bab49133f4edb19132d42214966f6
Miranda Bonilla <bonillamiranda3@gmail.com>
2019-05-24 17:54:10 +0000

    Automatically backed up by Learn

--- lib/concerns/modules.rb
+++ lib/concerns/modules.rb
@@ -1,15 +1,16 @@
 module Concerns
   module Findable
 
-    def find_by_name(name)
-      self.all.detect{|song| song.name == name}
-    end
-
-    def find_or_create_by_name(name)
-      if find_by_name == nil then create(name)
-      else find_by_name(name)
+      def find_by_name(name)
+        self.all.find {|song| song.name == name }
       end
-    end
+
+      def find_or_create_by_name(name)
+        if find_by_name(name) == nil then create(name)
+        else find_by_name(name)
+        end
+      end
   end
+end
 
 require_all 'lib'
